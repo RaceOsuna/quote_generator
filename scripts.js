8f461d80a498a36110b994c0c24da29c14ea4186
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2024-03-07 12:53:27 -0700

    Feat: fetch and display data/add functionality to twitter button

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+let twitterButton = document.getElementById('twitter');
+let getQuoteButton = document.getElementById('new-quote')
+let quoteData = [];
+
+function getRandomQuote() {
+  const newQuote = quoteData[Math.floor(Math.random() * quoteData.length)]
+  quote.textContent = newQuote.text;
+  author.textContent = `${newQuote.author}` || "-Unknown";
+}
+
+// function setQuoteAndAuthorText(quoteObj) {
+//   quote.innerText = quoteObj.text
+//   author.innerText = quoteObj.author
+// }
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    quoteData = await response.json();
+    getRandomQuote();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
+  window.open(twitterUrl, '_blank')
+}
+
+getQuoteButton.addEventListener('click', getRandomQuote)
+twitterButton.addEventListener('click', tweetQuote)
+
+// on load
+getQuotes()
+
